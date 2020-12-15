2abd5fc3b08dc16b141c142e967fa05474671ffd
CHIDILights <tomysean@naver.com>
2020-12-15 14:15:11 +0900

    Add overlay-resurrection

--- ef60.mk
+++ ef60.mk
@@ -15,8 +15,9 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-resurrection
 
 # Checking model
 PRODUCT_COPY_FILES += \
